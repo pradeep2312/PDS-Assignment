425c2d03d4063d0a0211c5556bfcbe1ceed98157
pradeep2312 <88088853+pradeep2312@users.noreply.github.com>
2023-02-27 19:54:43 -0600

    Add files via upload

---
+++ PDS assignment/question 1/Cleaned data/cleandata.R
@@ -0,0 +1,7 @@
+raw_yield_data <- read.csv("C:/Users/91938/Desktop/q1PDS.csv")
+View (raw_yield_data)
+#attach(raw_yield_data)
+cleaned_yield_data <- na.omit (raw_yield_data[raw_yield_data$Frailty !="NA",])
+View (cleaned_yield_data)
+
+plot(raw_yield_data$Height..inches,raw_yield_data$Weight..lbs,xlab="Height",ylab="Weight",main=("plot between height and weight"))
